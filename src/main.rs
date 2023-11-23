1b9012467f19af9d7b29c0fab58494e180caaab1
Navid Dezashibi <dezashibi@gmail.com>
2023-11-23 12:26:36 +0400

    player and player movement

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use crossterm::{
     ExecutableCommand,
 };
 use invaders::{
-    frame::{self, new_frame},
+    frame::{self, new_frame, Drawable},
+    player::Player,
     render,
 };
 use rusty_audio::Audio;
@@ -52,14 +53,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
 
     // Game Loop
+    let mut player = Player::new();
     'gameloop: loop {
         // Per-frame init
-        let curr_frame = new_frame();
+        let mut curr_frame = new_frame();
 
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -70,6 +74,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
 
         // Draw and Render
+        player.draw(&mut curr_frame);
         let _ = render_tx.send(curr_frame); // ignore errors rather than terminating the thread
         thread::sleep(Duration::from_millis(1));
     }
