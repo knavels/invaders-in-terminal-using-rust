75ab3f62dd74073b2963e7b766e6fa3bad226532
Navid Dezashibi <dezashibi@gmail.com>
2023-11-23 12:18:32 +0400

    game structure
    game loop, game control, render

---
+++ src/frame.rs
@@ -0,0 +1,17 @@
+use crate::{NUM_COLS, NUM_ROWS};
+
+pub type Frame = Vec<Vec<&'static str>>;
+
+pub fn new_frame() -> Frame {
+    let mut cols = Vec::with_capacity(NUM_COLS);
+    for _ in 0..NUM_COLS {
+        let col = vec![" "; NUM_ROWS];
+        cols.push(col);
+    }
+
+    cols
+}
+
+pub trait Drawable {
+    fn draw(&self, frame: &mut Frame);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-use std::error::Error;
+use std::{error::Error, io, sync::mpsc, thread, time::Duration};
 
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use invaders::{
+    frame::{self, new_frame},
+    render,
+};
 use rusty_audio::Audio;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -13,8 +23,64 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     audio.play("startup");
 
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Render loop in a separate thread
+    // channel to communicate
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        while let Ok(curr_frame) = render_rx.recv() {
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+        // better way is to use while let, I just keep this for anyone who might want to compare it
+        // loop {
+        //     let curr_frame = match render_rx.recv() {
+        //         Ok(x) => x,
+        //         Err(_) => break,
+        //     };
+        // render::render(&mut stdout, &last_frame, &curr_frame, false);
+        // last_frame = curr_frame;
+        // }
+    });
+
+    // Game Loop
+    'gameloop: loop {
+        // Per-frame init
+        let curr_frame = new_frame();
+
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        // Draw and Render
+        let _ = render_tx.send(curr_frame); // ignore errors rather than terminating the thread
+        thread::sleep(Duration::from_millis(1));
+    }
+
     // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
 
     Ok(())
 }
